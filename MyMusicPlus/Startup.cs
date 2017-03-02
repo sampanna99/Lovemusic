c662ee34525f5678fbcb8cc66de3286d8a6c81f4
sampanna pokhrel <sampanna99@gmail.com>
2017-03-01 19:18:53 -0800

    Add project files.

---
+++ MyMusicPlus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMusicPlus.Startup))]
+namespace MyMusicPlus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
