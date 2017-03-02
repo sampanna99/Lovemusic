1c96039ebe168ffb09ef2c85e873cb3eaf4d8baa
sampanna pokhrel <sampanna99@gmail.com>
2017-03-01 22:37:33 -0800

    added the dropdownlist

---
+++ MyMusicPlus/Controllers/GigsController.cs
@@ -0,0 +1,24 @@
+﻿using MyMusicPlus.Models;
+using MyMusicPlus.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MyMusicPlus.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel();
+            viewModel.Genres = _context.Genres.ToList();
+            return View(viewModel);
+        }
+    }
+}
---
+++ MyMusicPlus/Models/Gig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyMusicPlus.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+
+        public DateTime Datetime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+
+
+    }
+}
