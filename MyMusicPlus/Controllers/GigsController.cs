6081a818d3c35cbc6d537715e2d8ca1d48c6acf7
sampanna pokhrel <sampanna99@gmail.com>
2017-03-05 19:00:11 -0800

    added the gig

--- MyMusicPlus/Controllers/GigsController.cs
+++ MyMusicPlus/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
-﻿using MyMusicPlus.Models;
+﻿using Microsoft.AspNet.Identity;
+using MyMusicPlus.Models;
 using MyMusicPlus.ViewModel;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -9,16 +11,38 @@ namespace MyMusicPlus.Controllers
     {
         private ApplicationDbContext _context;
 
+
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
         // GET: Gigs
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel();
             viewModel.Genres = _context.Genres.ToList();
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            //var artist = _context.Users.Single(u => u.Id == artistId);
+            //var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                ArtistId = artistId,
+                Datetime = DateTime.Parse(string.Format("{0}{1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
--- MyMusicPlus/Models/Gig.cs
+++ MyMusicPlus/Models/Gig.cs
@@ -7,9 +7,11 @@ namespace MyMusicPlus.Models
     {
         public int Id { get; set; }
 
-        [Required]
+
         public ApplicationUser Artist { get; set; }
 
+        [Required]
+        public string ArtistId { get; set; }
 
         public DateTime Datetime { get; set; }
 
@@ -17,10 +19,11 @@ namespace MyMusicPlus.Models
         [StringLength(255)]
         public string Venue { get; set; }
 
-        [Required]
+
         public Genre Genre { get; set; }
 
-
+        [Required]
+        public byte GenreId { get; set; }
 
     }
 }
